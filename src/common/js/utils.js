6a0469e567901b246d528f41da6442a7b5eaa36b
liuyan <liuyan@yungengxin.com>
2019-09-04 18:33:57 +0800

    mocha

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -1,3 +1,16 @@
-export function add(a,b){
-  return a+b;
+export function add (a, b) {
+  return a + b;
 }
+export async function asyncFn () {
+  let a = 2;
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('::???');
+      a += 4;
+      resolve();
+    }, 1000);
+  });
+  console.log(':::aaa', a);
+  return a;
+}
+// console.log(asyncFn());
--- test/test.js
+++ test/test.js
@@ -1,10 +1,17 @@
-import { add } from '../src/common/js/utils.js';
+import { add, asyncFn } from '../src/common/js/utils.js';
 const expect = require('chai').expect;
-describe('加法函数测试', () => {
-  it('add方法应该返回2', () => {
+// describe块称为"测试套件"（test suite），表示一组相关的测试。它是一个函数，第一个参数是测试套件的名称（"加法函数的测试"），第二个参数是一个实际执行的函数
+describe('加法函数测试', function () {
+  it('add方法应该返回2', function () {
     expect(add(1, 1)).to.be.equal(2);
   });
-  it('add方法应该返回3', () => {
-    expect(add(1, 2)).to.be.equal(4);
+  it('add方法应该返回3', function () {
+    expect(add(1, 2)).to.be.equal(3);
+  });
+});
+describe('测试异步', function () {
+  it('返回6', async function () {
+    let r = await asyncFn();
+    expect(r).to.be.equal(6);
   });
 });
