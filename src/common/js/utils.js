dc09097de6cad2420c9a4add68f10057e0ae56ba
liuyan <liuyan@yungengxin.com>
2019-09-05 09:44:26 +0800

    使用mochawesome模块，生成html的测试报告

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -5,12 +5,9 @@ export async function asyncFn () {
   let a = 2;
   await new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log('::???');
       a += 4;
       resolve();
     }, 1000);
   });
-  console.log(':::aaa', a);
   return a;
 }
-// console.log(asyncFn());
