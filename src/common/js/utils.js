3ed8e098c309eda1704b3dc48907b504f9c95530
liuyan <liuyan@yungengxin.com>
2020-01-07 10:04:09 +0800

    1

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -5,9 +5,12 @@ export async function asyncFn () {
   let a = 2;
   await new Promise((resolve, reject) => {
     setTimeout(() => {
+      console.log('::???');
       a += 4;
       resolve();
     }, 1000);
   });
+  console.log(':::aaa', a);
   return a;
 }
+// console.log(asyncFn());
